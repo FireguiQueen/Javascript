9b2228ff1edf014d6d7c807d78462e87b8774b3f
Guilherme <Fireguiquenbr@gmail.com>
2023-04-11 19:17:35 -0300

    Update console-log.js

--- 1 - Javascript básico/Excercises/console-log.js
+++ 1 - Javascript básico/Excercises/console-log.js
@@ -1,1 +1,6 @@
-console.log(`Olá, meu nome é "Guilherme" e estou estudando às ${7} horas da noite.`)
+// utilizando template strings.:
+console.log(`Olá, meu nome é "Guilherme" e estou estudando às ${7} horas da noite.`);
+
+
+// utilizando aspas simples.: 
+console.log('Olá, meu nome é "Guilherme" e estou estudando às ' + 7 + ' horas da noite.')
