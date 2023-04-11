7d0622807af499d32de2e9e013303ac29c16e155
Guilherme <Fireguiquenbr@gmail.com>
2023-04-11 19:06:18 -0300

    Update aspas, aspas suplas & crase.js

--- 1 - Javascript básico/aspas, aspas suplas & crase.js
+++ 1 - Javascript básico/aspas, aspas suplas & crase.js
@@ -8,12 +8,23 @@ Em relação as aspas, aspas duplas e crase, não há NENHUMA diferença em ques
 Você é livre para escolher qual será utilizada, mas há um porém.
 */
 
+
 // Muita das vezes sua string irá precisar possuir aspas simples (''). Sendo assim,
 // você será obrigado a utilizar outro caracter para formar a string, como no exemplo abaixo.
 console.log("He's a nice guy");
+
 
 
 // Em outros casos, seu string irá precisar armazenar aspas duplas e aspas simples, então
 // você será obrigado a utilizar crase (``) para formar a string.
 // Pois perceba que a frase já possui aspas duplas e simples. 
 console.log(`Last year he told me "I'll be rich, like a monkey"`)
+
+// As crases também são importantes para chamar variáveis. Através delas, podemos usar ${} e chamar uma variável.
+
+// Método convencional:
+const frase_1 = "Valar Morghulis..";
+console.log('A frase' + frase_1 + 'é do Game of Thrones, uma série maravilhosa!');
+
+// Método "avançado" (template strings):
+const frase_2
