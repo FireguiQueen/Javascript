c48f32b6dae9eb1be2986d925db559cb29d69924
Guilherme <Fireguiquenbr@gmail.com>
2023-04-11 18:59:28 -0300

    Update

---
+++ 1 - Javascript básico/aspas, aspas suplas & crase.js
@@ -0,0 +1,14 @@
+console.log('HHM');
+console.log("HHM");
+console.log(`HHM`);
+
+/*
+OBS:
+Em relação as aspas, aspas duplas e crase, não há NENHUMA diferença em questões de memória.
+Você é livre para escolher qual será utilizada, mas há um porém.
+*/
+
+// Muita das vezes sua string irá precisar possuir aspas simples (''). Sendo assim,
+// você será obrigado a utilizar outro caracter para formar a string, como no exemplo abaixo.
+console.log("He's a nice guy");
+
---
+++ 1 - Javascript básico/comments.js
@@ -0,0 +1,8 @@
+// Comentário em linha
+// Só é possível uma linha por comentário 
+
+
+/*  
+    Comentário onde podemos utilizar de diversas linhas
+    até fechar o mesmo usando asterísco (*) e barra ().
+*/
