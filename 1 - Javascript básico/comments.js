e9b3f3e4aee60c2609d3369fa6394f63e68262fa
Guilherme <Fireguiquenbr@gmail.com>
2023-04-11 20:35:14 -0300

    update

--- 1 - Javascript básico/comments.js
+++
@@ -1,8 +0,0 @@
-// Comentário em linha
-// Só é possível uma linha por comentário 
-
-
-/*  
-    Comentário onde podemos utilizar de diversas linhas
-    até fechar o mesmo usando asterísco (*) e barra ().
-*/
