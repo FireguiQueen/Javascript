aa039fdd051589eb6bd36f1bdc1f3c235975de2d
Guilherme <Fireguiquenbr@gmail.com>
2023-04-13 19:02:31 -0300

    Create const & let.js

---
+++ 1 - Javascript básico/Excercises/const & let.js
@@ -0,0 +1,16 @@
+/* 
+(pessoa) tem (n) anos, pesa (n)kg.
+Tem (n) de altura, e seu IMC é de (n).
+(pessoa) nasceu em (n) ano.
+*/
+
+
+const nome = 'Guilherme P';
+const altura = '1.74 cm';
+const peso = '68 kg';
+let imc = peso / (altura**2) // peso / (altura²)
+
+console.log(`${imc}
+
+
+`)
