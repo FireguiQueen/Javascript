b553954f2bdfd1d462d22070f52f7d295fbad639
Guilherme <Fireguiquenbr@gmail.com>
2023-04-16 21:09:41 -0300

    Update aspas, aspas suplas & crase.js

--- 1 - Javascript básico/aspas, aspas suplas & crase.js
+++ 1 - Javascript básico/aspas, aspas suplas & crase.js
@@ -4,8 +4,8 @@ console.log(`HHM`);
 
 /*
 OBS:
-Em relação as aspas, aspas duplas e crase, não há NENHUMA diferença em questões de memória.
-Você é livre para escolher qual será utilizada, mas há um porém.
+    Em relação as aspas, aspas duplas e crase, não há NENHUMA diferença em questões de memória/eficiência.
+    Você é livre para escolher qual será utilizada, mas há um porém.
 */
 
 
