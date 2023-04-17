e3d865dc85eae5dbc7d9fcb84ce8bdf712ca3bbc
Guilherme <Fireguiquenbr@gmail.com>
2023-04-17 19:47:49 -0300

    Update string.js

--- 1 - Javascript básico/Excercises/string.js
+++ 1 - Javascript básico/Excercises/string.js
@@ -1,9 +1,9 @@
 // Faça uma variável com uma frase. A string será usando aspas simples, e nesta mesma frase você precisa colocar aspas simples.
-const frase = 'Walter isn\'t enough for our company in Albuquerque.'
+const frase = 'Waltuh, put your dick away, i\'m not having sex with you right now Waltuh'
 console.log(frase)
 
 // Faça um split desta usando o parâmetro ('a'). Antes de ver o resultado, escreva-o abaixo.:
 // Se errar, pense em outra frase, e faça isto novamente.
-const resultadoPrevisto = ['W', "lter isn't enough for our comp", 'ny in ', 'albuquerque.']
+const resultadoPrevisto = ["W", "ltuh, put your dick ", "w", "y, i'm not h", "ving sex with your right now W", "ltuh"]
 console.log(frase.split('a'))
 
