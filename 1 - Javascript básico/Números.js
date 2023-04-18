d95b6da5754e92fe5a6a0e93995197f7033ce479
Guilherme <Fireguiquenbr@gmail.com>
2023-04-18 20:20:04 -0300

    Create Números.js

---
+++ 1 - Javascript básico/Números.js
@@ -0,0 +1,5 @@
+//          Arredondando valores
+// Podemos imaginar que "toFixed" significar "fixar". Ou seja, ficar um número de casas decimais.
+const num1 = 3.14159265
+console.log(num1.toFixed(2)) // dentro dos parentesis colacamos quantas casas queremos que apareça
+
