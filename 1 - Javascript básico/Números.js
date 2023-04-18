e775838cfa706c90bad1818e180e02ab8e964bbd
Guilherme <Fireguiquenbr@gmail.com>
2023-04-18 20:25:04 -0300

    Update Números.js

--- 1 - Javascript básico/Números.js
+++ 1 - Javascript básico/Números.js
@@ -1,5 +1,13 @@
-//          Arredondando valores
+//                                  ARREDONDANDO VALORES
 // Podemos imaginar que "toFixed" significar "fixar". Ou seja, ficar um número de casas decimais.
 const num1 = 3.14159265
 console.log(num1.toFixed(2)) // dentro dos parentesis colacamos quantas casas queremos que apareça
+
+
+//                              VERIFICANDO SE O NÚMERO É INTEIRO
+// Acessamos o objeto "number", depois acessamos o método "isInteger" e colocamos qual valor
+// queremos saber se é inteiro ou não. 
+const num2 = 7
+console.log(Number.isInteger(num2));
+
 
