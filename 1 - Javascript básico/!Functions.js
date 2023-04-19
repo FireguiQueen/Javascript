6dc044c056ff3e47b77ce01620c2b2a6451a565a
Guilherme <Fireguiquenbr@gmail.com>
2023-04-19 19:13:27 -0300

    Update !Functions.js

---
+++ 1 - Javascript básico/!Functions.js
@@ -0,0 +1,27 @@
+//  O QUE É UMA FUNÇÃO
+/*
+    Uma função é como se fosse um sub-programa dentro de um programa.
+    Através dela podemos fazer com que o nosso programa excute determinada ação ou retorne 
+    algum valor.
+
+    Reutilização de código: As funções permitem que você escreva um bloco de código que pode ser reutilizado várias vezes em diferentes partes do seu programa. Isso pode economizar muito tempo e esforço, além de tornar seu código mais modular e organizado.
+
+    Abstração: As funções podem encapsular complexidade e abstrair detalhes do seu código, tornando-o mais fácil de entender e manter. Isso pode ser particularmente útil para funções que realizam tarefas complexas ou repetitivas.
+
+    Modularidade: As funções podem ser combinadas para criar programas maiores e mais complexos. Isso torna seu código mais modular e fácil de entender, permitindo que você se concentre em partes específicas do seu programa sem se preocupar com os detalhes do resto.
+
+    Escopo: As funções têm seu próprio escopo, o que significa que as variáveis ​​definidas dentro de uma função não afetam as variáveis ​​definidas fora dela. Isso ajuda a evitar conflitos e erros no seu código.
+*/
+
+// Entendendo uma função
+/*
+    Primeiramente nós declaramos uma função utilizando a keyword "function".
+    Depois colocamos um nome para esta função. As regras para criar o nome são as mesmas dos nomes das variáveis.
+    Após o nome, abrimos e fechamos parêntesis '()'. 
+    Não somos obrigados a colocar nada nestes parêntesis, mas o que colocamos são chamados de "" 
+
+
+// Function declaration
+function nomeDaFuncao(){
+
+}
