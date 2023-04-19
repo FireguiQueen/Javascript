96154dc451b6159c04cf5125df33d00baa3a8ee6
Guilherme <Fireguiquenbr@gmail.com>
2023-04-18 22:28:41 -0300

    Update math.js

---
+++ 1 - Javascript básico/Excercises/math.js
@@ -0,0 +1,21 @@
+// *** ESCOLHA UM NÚMERO E.:
+// Mostre a raiz quadrada;
+// Se é inteiro ou não;
+// Se é NaN ou não;
+// Arredonde para baixo;
+// Arredonde para cima;
+// Deixe apenas duas casas decimais.
+
+const num = 9;
+
+const squareRoot = num ** 0.5;
+const itIsInteger = Number.isInteger(num);
+console.log(`
+Raiz quadrada: ${parseInt(squareRoot)}
+Seu número ${itIsInteger? 'é inteiro' : 'não é inteiro'}
+${Number.isNaN? 'Seu número não é \'NaN\'' : 'Seu número é \'NaN\''}
+Número arredondado para baixo: ${itIsInteger? 'impossível arredondar pois é um número inteiro' : Math.floor(num)}
+Número arredondado para cima: ${itIsInteger? 'impossível arredondar pois é um número inteiro' : Math.ceil(num)}
+Número com duas casas decimais: ${num.toFixed(2)}
+
+`)
