7bb176b770f1a911853ffdab0789b23da515f767
Guilherme <Fireguiquenbr@gmail.com>
2023-04-19 22:08:10 -0300

    Update !Objects.js

---
+++ 1 - Javascript básico/!Objects.js
@@ -0,0 +1,22 @@
+// O QUE SÃO
+/*
+    Objetos em JS são como os da vida real.
+    Pense em uma caneca. 
+    Uma caneca é um objeto, e esta mesma caneca possui diversos atributos, tais como.:
+    cor, espessura, largura, altura, material e assim por diante..
+
+    No Javascript, também utilizamos dos objetos para acessar informações que irão possuir 
+    atributos.
+
+    Nos objetos temos propriedades, valores e métodos.
+    As propriedades são os nomes dos atributos, por exemplo a palavra 'cor' é uma
+    propriedade.
+    Valores são os valores que serão dados as propriedades, por exemplo a palavra 'verde' é o valor
+    da propriedade 'cor'.
+    E por fim, os métodos são funções que estão dentro de objetos.
+*/
+
+const pessoa_01 = {    // Utilize chaves '{}' para inicializar um objeto
+    nome: 'Guilherme', // 'nome' é a propriedade, 'Guilherme' é o valor.
+    idade: 17,         
+}
