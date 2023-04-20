daf79c5f05ab538fc7cb98dc7c7569f6946a8d6f
Guilherme <Fireguiquenbr@gmail.com>
2023-04-20 11:16:40 -0300

    Update !Arrays.js

--- 1 - Javascript básico/!Arrays.js
+++ 1 - Javascript básico/!Arrays.js
@@ -8,9 +8,11 @@
     momento.
 */
 
+
 //                      FAZENDO UM ARRAY
 // Indices:     0       1        2
 let pessoas = ['Pablo', 'Flora', 'Nathalia'];
+
 
 
 
@@ -19,8 +21,10 @@ console.log(pessoas[0])
 
 
 
+
 //                     QUANTANDO QUANTOS ITENS O ARRAY POSSUI
 console.log(pessoas.length)
+
 
 
 
@@ -31,15 +35,20 @@ console.log(pessoas)
 
 
 
+
 //                     EMPURRANDO (adicionando) UM OU MAIS ITENS AO FINAL DO ARRAY
 // Neste exemplo empurramos dois itens: 'alice' e 'juju'.. Mas podemos empurrar quantos itens quisermos
 pessoas.push('Alice', 'Juju');
 console.log(pessoas)
 
 
+
 //                    REMOVENDO UM ITEM AO FINAL DO ARRAY
 pessoas.pop()
 console.log(pessoas)
+
+
+
 
 
 //                   ADICIONANDO UM OU MAIS ITENS AO INÍCIO DO ARRAY
@@ -55,6 +64,8 @@ console.log(pessoas)
 
 
 
+
+
 //                  REMOVENDO UM ITEM ESPECÍFICO E SEM ALTERAR OS INDÍCES
 // Indices:    0       1        2         3    
 const nomes = ['Hugo', 'Maria', 'Gaspar', 'Cleiton'];
@@ -63,7 +74,8 @@ console.log(nomes)
 
 
 
-//                 PEGANDO INDICES DESEJADOS
+
+//                          PEGANDO INDICES DESEJADOS
 // Useremos o "slice". Vamos "fatiar" quais itens do array nós queremos.
 // Indices:        0               1        2            3             4
 const usernames = ['FireguiQueen', 'x_2KB', 'kovadetha', 'MaeCareca_', 'Darknesswarrior']
