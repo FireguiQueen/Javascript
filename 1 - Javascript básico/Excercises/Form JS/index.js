ace92eafa0260e6313a3217615ba2b40af8cf7e2
Guilherme <Fireguiquenbr@gmail.com>
2023-04-28 11:52:42 -0300

    update

---
+++ 1 - Javascript básico/Excercises/Form JS/index.js
@@ -0,0 +1,10 @@
+function InvalidMsg(textbox) {
+
+    if(textbox.validity.patternMismatch){
+       textbox.setCustomValidity('please enter 10 numeric value.');
+   }    
+   else {
+       textbox.setCustomValidity('');
+   }
+   return true;
+}
