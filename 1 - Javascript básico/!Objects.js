b9e19dc2486208e5282527bb7cccf0a5bac91e4c
Guilherme <Fireguiquenbr@gmail.com>
2023-05-02 09:17:56 -0300

    Update !Objects.js

--- 1 - Javascript básico/!Objects.js
+++ 1 - Javascript básico/!Objects.js
@@ -6,14 +6,15 @@
     cor, espessura, largura, altura, material e assim por diante..
 
     No Javascript, também utilizamos dos objetos para acessar informações que irão possuir 
-    atributos.
+    propriedades e os valores dessas propriedades.
 
     Nos objetos temos propriedades, valores e métodos.
     As propriedades são os nomes dos atributos, por exemplo a palavra 'cor' é uma
     propriedade.
     Valores são os valores que serão dados as propriedades, por exemplo a palavra 'verde' é o valor
     da propriedade 'cor'.
-    E por fim, os métodos são funções que estão dentro de objetos.
+    E por fim, os métodos são funções que estão dentro de objetos. Podemos imaginar que seria algo como.:
+    Se você encostar na caneca, ela irá ascender um LED laranja. 
 */
 
 const pessoa_01 = {         // Utilize chaves '{}' para inicializar um objeto
