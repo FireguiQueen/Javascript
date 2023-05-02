91c8c9cce80341496f57afe853c12c86434a26da
Guilherme <Fireguiquenbr@gmail.com>
2023-05-02 12:27:32 -0300

    Update index.js

--- 1 - Javascript básico/Excercises/Form JS/index.js
+++ 1 - Javascript básico/Excercises/Form JS/index.js
@@ -1,10 +1,25 @@
 const formulario = document.getElementById('meu-formulario');
+const pessoas = [];
 
 formulario.addEventListener('submit', function(evento){
     evento.preventDefault();
-
+    
+    const nome = document.querySelector('fieldset input#name').value
+    const idade = document.querySelector('fieldset input#age').value
+    const altura = document.querySelector('fieldset input#altura').value
+    const peso = document.querySelector('fieldset input#peso').value
+    
+    function newPerson(name, age, height, weight){
+        return{
+            name,
+            age,
+            height,
+            weight
+        }
+    }
+    pessoas.push(newPerson(nome, idade, altura, peso))
+    console.log(pessoas)
     const inputs = document.querySelectorAll('input');
     inputs.forEach(e => e.value = '')
 
-    const nome = document.querySelector('fieldset input#name')
 })
