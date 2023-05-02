98e4f3a63c950e8f989bfc42e08869b84808fe4c
Guilherme <Fireguiquenbr@gmail.com>
2023-05-02 19:01:24 -0300

    Update index.js

--- 1 - Javascript básico/Excercises/Form JS/index.js
+++ 1 - Javascript básico/Excercises/Form JS/index.js
@@ -18,11 +18,11 @@ formulario.addEventListener('submit', function(evento){
         }
     }
     pessoas.push(newPerson(nome, idade, altura, peso))
-    console.log(pessoas)
-    const inputs = document.querySelectorAll('input[type=text]');
-    inputs.forEach(e => e.value = '')
 
-    // 
+    const inputs = document.querySelectorAll('input[type=text]');
+    inputs.forEach(e => e.value = '');
+
+    
     const imc = peso / (altura ** 2);
     document.getElementById('resultado').innerHTML = `
     Seu nome é ${nome}, e seu IMC é.: ${imc.toFixed(0)}
