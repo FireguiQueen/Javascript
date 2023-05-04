788052f9538515e87361a14bb56837c971d89415
Guilherme <Fireguiquenbr@gmail.com>
2023-05-03 22:43:37 -0300

    Update !Arrays.js

--- 1 - Javascript básico/!Arrays.js
+++ 1 - Javascript básico/!Arrays.js
@@ -2,7 +2,10 @@
 /* 
     Arrays são estruturas de dados usadas em JavaScript para armazenar e acessar coleções de 
     valores de forma organizada. Um array é uma lista ordenada de elementos que podem ser números, strings, 
-    objetos ou outros arrays. Cada elemento é identificado por um índice, que começa em 0 para o 
+    objetos ou outros arrays.. 
+    -> Em suma, um array é uma lista e pode armazenar qualquer outro tipo de dado.
+    
+    Cada elemento é identificado por um índice, que começa em 0 para o 
     primeiro elemento e aumenta em 1 para cada elemento subsequente. Os arrays em JavaScript são 
     dinâmicos, o que significa que você pode adicionar e remover elementos de um array a qualquer
     momento.
