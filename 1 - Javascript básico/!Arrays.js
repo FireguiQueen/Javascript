ccda14c651ba8ee16542a57975fb821224afae0a
Guilherme <Fireguiquenbr@gmail.com>
2023-05-22 22:23:54 -0300

    Update !Arrays.js

--- 1 - Javascript básico/!Arrays.js
+++ 1 - Javascript básico/!Arrays.js
@@ -37,7 +37,6 @@ console.log(pessoas);
 
 
 
-
 //                 EMPURRANDO (adicionando) UM OU MAIS ITENS AO FINAL DO ARRAY
 // Neste exemplo empurramos dois itens: 'alice' e 'juju'.. Mas podemos empurrar quantos itens quisermos
 pessoas.push('Alice', 'Juju');
