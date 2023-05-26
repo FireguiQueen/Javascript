39e80c877f669dbb5de7b7ab367da006708296de
Guilherme <Fireguiquenbr@gmail.com>
2023-05-25 22:34:32 -0300

    Update app.js

--- 2 - Lógica de programação/Exercises/People-info/assets/app.js
+++ 2 - Lógica de programação/Exercises/People-info/assets/app.js
@@ -5,18 +5,21 @@ const people = [];
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    sendInf();
+    clearInputs();
+});
 
 
+// Captura informações dos inputs e envia para um objeto
+function sendInf(){
     const input1 = document.getElementsByClassName('inf')[0].value;
     const input2 = document.getElementsByClassName('inf')[1].value;
     const input3 = document.getElementsByClassName('inf')[2].value;
     const input4 = document.getElementsByClassName('inf')[3].value;
     people.push(takeInformations(input1, input2, input3, input4));
+}
 
-    clearInputs();
-});
-
-// Captura informações da pessoa
+// Armezana informações capturadas anteriormente
 function takeInformations(name, age, country, email){
     return{
         name,
