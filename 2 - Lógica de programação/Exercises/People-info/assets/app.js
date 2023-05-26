c18774a1747ae94a0596c768567a9f4fe2c2c5ca
Guilherme <Fireguiquenbr@gmail.com>
2023-05-25 21:57:49 -0300

    JS

--- 2 - Lógica de programação/Exercises/People-info/assets/app.js
+++ 2 - Lógica de programação/Exercises/People-info/assets/app.js
@@ -2,4 +2,17 @@ const form = document.querySelector('body main form');
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+
+    clearInputs();
 });
+
+
+function takeInformations(name, age, ){
+
+}
+
+// Limpa os valores colocados pelo usuário
+function clearInputs(){
+    const allInputs = document.querySelectorAll('body main form input');
+    allInputs.forEach(e => e.value = '')
+}
