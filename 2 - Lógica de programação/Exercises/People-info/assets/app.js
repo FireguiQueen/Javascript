8c26e9282784ee40c50bef1fbb96092a2c709a36
Guilherme <Fireguiquenbr@gmail.com>
2023-05-25 22:10:27 -0300

    Update app.js

--- 2 - Lógica de programação/Exercises/People-info/assets/app.js
+++ 2 - Lógica de programação/Exercises/People-info/assets/app.js
@@ -1,14 +1,21 @@
 const form = document.querySelector('body main form');
 
+// Armazena as informações das pessoas
+const people = [];
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
+
+
+    const inputsInf = document.querySelectorAll('body main form input')
+    inputsInf.forEach(e => takeInformations(e.value))
 
     clearInputs();
 });
 
 
 function takeInformations(name, age, country, email){
-    
+
 }
 
 // Limpa os valores colocados pelo usuário
