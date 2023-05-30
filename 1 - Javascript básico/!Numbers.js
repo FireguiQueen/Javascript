1c503b0b4e34796b2d6098218824b2ecdca127e7
Guilherme <Fireguiquenbr@gmail.com>
2023-05-29 22:06:40 -0300

    Update !Numbers.js

--- 1 - Javascript básico/!Numbers.js
+++ 1 - Javascript básico/!Numbers.js
@@ -5,7 +5,6 @@ console.log(num1.toFixed(2)) // dentro dos parentesis colacamos quantas casas qu
 
 
 
-
 //                              VERIFICANDO SE O NÚMERO É INTEIRO
 // Acessamos o objeto "number", depois acessamos o método "isInteger" e colocamos qual valor
 // queremos saber se é inteiro ou não. 
