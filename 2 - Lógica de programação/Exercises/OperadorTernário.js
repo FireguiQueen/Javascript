1389f5e5d0140130abe494f6b9e47bac231c8705
Guilherme <Fireguiquenbr@gmail.com>
2023-06-02 22:26:31 -0300

    Update OperadorTernário.js

--- 2 - Lógica de programação/Exercises/OperadorTernário.js
+++ 2 - Lógica de programação/Exercises/OperadorTernário.js
@@ -1,11 +1,17 @@
 // Crie códigos usando o operador ternário '?' para substituir as estruturas condicionais abaixo.:
 
+// Objeto base
 const pessoa_01 = {
     name: 'Otávio',
     idade: 24
 }
+
+// Substitua esta estrutura condicional.:
 if(pessoa_01.idade >= 18){
     console.log('Você é de maior.')
 } else{
-    console.log('Você é de menor')
+    console.log('Você é de menor.')
 }
+
+// Resposta.:
+console.log(pessoa_)
