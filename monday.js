a6a58b449485a9c0caf713af766bb89c584c6e0f
Guilherme <Fireguiquenbr@gmail.com>
2023-06-05 19:44:07 -0300

    Update monday.js

--- monday.js
+++ monday.js
@@ -11,3 +11,15 @@ console.log(lista.length);
 // Altere um item
 lista[2] = 'R2D2'
 console.log(lista)
+
+// +1 item ao final da lista
+
+
+// -1 item ao final da lista
+
+
+// +1 item ao inicio 
+
+
+
+//-1 item ao incio
