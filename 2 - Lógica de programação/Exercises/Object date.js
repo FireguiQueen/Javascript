2c06f84b8661873dd6bccf721263c4d552786174
Guilherme <Fireguiquenbr@gmail.com>
2023-06-07 21:09:57 -0300

    Update Object date.js

--- 2 - Lógica de programação/Exercises/Object date.js
+++ 2 - Lógica de programação/Exercises/Object date.js
@@ -1,15 +1,16 @@
 function greetings(){
-    const date = new Date(2023, 5, 7, 1);
+    const date = new Date();
     const hour = date.getHours();
     // 'Else if' não são necessários pois estamos utiliando 'return'. 
     // Utilizá-lo apenas iria poluir o código.
 
     function formatarHora(hora){
-        const formatar = hora < 10? `0${hora}` : hora;
-        return formatar;
+        const horaFormatada = hora < 10? `0${hora}` : hora;
+        const formatarMais = horaFormatada == 1? `São ${horaFormatada} hora.` : `São ${horaFormatada} horas`
+        return formatarMais;
     }
 
-    const sayTime = `São ${formatarHora(hour)} hora(s).`
+    const sayTime = formatarHora(hour);
     if (hour >= 12 && hour < 18) return `Boa tarde! ${sayTime}`;
     if (hour >= 18 && hour < 24) return `Boa noite! ${sayTime}`;
     if (hour >= 00 && hour < 06) return `Que madrugada, hein! ${sayTime}`
