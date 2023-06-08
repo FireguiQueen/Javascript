c6c7bbf310f9980b10f89bdc4ca889b3c67fd917
Guilherme <Fireguiquenbr@gmail.com>
2023-06-07 21:13:18 -0300

    Update Object date.js

--- 2 - Lógica de programação/Exercises/Object date.js
+++ 2 - Lógica de programação/Exercises/Object date.js
@@ -13,6 +13,6 @@ function greetings(){
     if (hour >= 12 && hour < 18) return  `Boa tarde! ${sayTime}`;
     if (hour >= 18 && hour < 24) return  `Boa noite! ${sayTime}`;
     if (hour >= 00 && hour < 06) return  `Que madrugada, hein! ${sayTime}`
-    if (hour >= 06 && hour < 12) return   `Bom dia! ${sayTime}`
+    if (hour >= 06 && hour < 12) return  `Bom dia! ${sayTime}`
 }
 console.log(greetings())
