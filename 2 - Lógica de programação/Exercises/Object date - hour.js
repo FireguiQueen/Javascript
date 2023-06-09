4d1e51b2ff050bc9ab2d03cf7b50a4c92fd7a514
Guilherme <Fireguiquenbr@gmail.com>
2023-06-08 21:50:36 -0300

    Update Object date - hour.js

--- 2 - Lógica de programação/Exercises/Object date - hour.js
+++ 2 - Lógica de programação/Exercises/Object date - hour.js
@@ -1,7 +1,8 @@
-const date = new Date();
+const date = new Date(2019, 1, 2, 1, 25);
 const hour = date.getHours();
 
 function AddZeroToHour(hour){
-    const addingZero = hour < 10? `` : 1;
-    return addingZero
+    const addingZero = hour < 10? `0${hour}` : hour;
+    return addingZero;
 }
+console.log(AddZeroToHour(hour))
