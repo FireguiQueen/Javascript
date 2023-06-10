d2e50765f61dafa7474cab79c04d83ac84fdfed3
Guilherme <Fireguiquenbr@gmail.com>
2023-06-09 23:05:34 -0300

    Update Switch case & object date.js

--- 2 - Lógica de programação/Exercises/Switch case & object date.js
+++ 2 - Lógica de programação/Exercises/Switch case & object date.js
@@ -3,7 +3,7 @@ function sayWeekDay(day){
     const weekDays = [
         'Domingo', 'Segunda-feira', 'Terça-feira', 
         'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'
-    ]
+    ];
     switch(day){
         case 0: 
             return weekDays[0];        
