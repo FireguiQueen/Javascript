1d91c8f77d3c52edab01048e2abdafa3fbc924f8
Guilherme <Fireguiquenbr@gmail.com>
2023-06-12 21:13:35 -0300

    Update !Objects.js

--- 1 - Javascript básico/!Objects.js
+++ 1 - Javascript básico/!Objects.js
@@ -26,3 +26,4 @@ const pessoa_01 = {         // Utilize chaves '{}' para inicializar um objeto
     idade: 14,              // 'idade' é a propriedade, 14 é o valor.        
 }
 console.log(pessoa_01.nome) // Para acessar alguma propriedade do objeto, utilize um ponto final (.)
+
