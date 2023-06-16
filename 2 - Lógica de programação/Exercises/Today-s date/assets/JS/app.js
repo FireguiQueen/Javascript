1b095dcdf07f44cf968f2dfa40b08f00fc57ffa3
Guilherme <Fireguiquenbr@gmail.com>
2023-06-15 23:50:37 -0300

    Update app.js

--- 2 - Lógica de programação/Exercises/Today-s date/assets/JS/app.js
+++ 2 - Lógica de programação/Exercises/Today-s date/assets/JS/app.js
@@ -8,33 +8,42 @@ function todaySDate(dayWeek = 'Segunda-feira', dayMonth = '1', month = 'janeiro'
 }
 
 function dayOfTheWeek(){
+    const dayWeek = [
+        'Domingo',
+        'Segunda-feira',
+        'Terça-feira',
+        'Quarta-feira',
+        'Quinta-feira',
+        'Sexta-feira',
+        'Sábado'
+    ]
     switch(date.getDay()){
         // Break it's necessary bcs we're using a function that uses 'return'.
         case 0: 
-            return 'Domingo';
+            return dayOfTheWeek[0];
     
         case 1:
-            return 'Segunda-feira';
+            return dayOfTheWeek[1];
     
         case 2: 
-            return 'Terça-feira';
+            return dayOfTheWeek[2];
     
         case 3: 
-            return 'Quarta-feira';
+            return dayOfTheWeek[3];
     
         case 4: 
-            return 'Quinta-feira';
+            return dayOfTheWeek[4];
         
         case 5: 
-            return 'Sexta-feira'
+            return dayOfTheWeek[5]
     
         case 6:
-            return 'Sábado';
+            return dayOfTheWeek[6];
     }
 }
 
 function correctMonth(){
-    
+
 }
 
 console.log(todaySDate())
