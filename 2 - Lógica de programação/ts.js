352e9de0451534ea614f9075c37cb9bddd646b7e
Guilherme <Fireguiquenbr@gmail.com>
2023-06-15 23:30:11 -0300

    Update ts.js

--- 2 - Lógica de programação/ts.js
+++ 2 - Lógica de programação/ts.js
@@ -5,16 +5,16 @@ let data_com_ano_alterado = new Date('2023-06-06 22:32:21');
 
 console.log(data_com_ano_alterado);
 
-function checkNumber(numero){
-    const test = numero >= 1;
+const pessoa = 'j';
+switch(pessoa){
+    case 'Maria':
+        console.log('Seu nome é Maria, e rima com coringa!');
+        break;
 
-    switch(numero){
-        case test: 
-            return 'Seu número é maior ou igual a um.'
-            break;
-    
-        default:
-            return 'Seu número é menor do que um.'
-    }
+    case 'joao':
+        console.log('Seu nome é João, e rima com anão!');
+        break;
+
+    default:
+        console.log('Nenhum nome foi digitado ou o nome passado não possui informaões');
 }
-console.log(checkNumber(2))
