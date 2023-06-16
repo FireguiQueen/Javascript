2293fcabf3e7907f4f161daf05ebbbe2e13bb128
Guilherme <Fireguiquenbr@gmail.com>
2023-06-15 23:38:33 -0300

    Update app.js

---
+++ 2 - Lógica de programação/Exercises/Today-s date/assets/JS/app.js
@@ -0,0 +1,28 @@
+// captura o dia de hoje 
+const date = new Date();
+
+function sayDayOfTheWeek(){
+    switch(date.getDay()){
+        case 0: 
+            return 'Domingo';
+    
+        case 1:
+            return 'Segunda-feira';
+    
+        case 2: 
+            return 'Terça-feira';
+    
+        case 3: 
+            return 'Quarta-feira';
+    
+        case 4: 
+            return 'Quinta-feira';
+        
+        case 5: 
+            return 'Sexta-feira'
+    
+        case 6:
+            return 'Sábado';
+    }
+}
+
