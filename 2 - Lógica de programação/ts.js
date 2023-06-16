2157cef577ef4caca3b89b6acb78dc0d50e04492
Guilherme <Fireguiquenbr@gmail.com>
2023-06-15 23:27:00 -0300

    Update ts.js

--- 2 - Lógica de programação/ts.js
+++ 2 - Lógica de programação/ts.js
@@ -4,3 +4,17 @@ let data_com_ano_alterado = new Date('2023-06-06 22:32:21');
 // let data_com_ano_alterado = new Date('2023-06-06T22:32:21');
 
 console.log(data_com_ano_alterado);
+
+function checkNumber(numero){
+    const test = numero >= 1;
+
+    switch(numero){
+        case test: 
+            return 'Seu número é maior ou igual a um.'
+            break;
+    
+        default:
+            return 'Seu número é menor do que um.'
+    }
+}
+console.log(checkNumber(2))
