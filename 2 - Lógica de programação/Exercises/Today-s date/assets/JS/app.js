1940faf6e3ad4bdcf6819f001318c40daac70928
Guilherme <Fireguiquenbr@gmail.com>
2023-06-16 23:40:13 -0300

    update

--- 2 - Lógica de programação/Exercises/Today-s date/assets/JS/app.js
+++ 2 - Lógica de programação/Exercises/Today-s date/assets/JS/app.js
@@ -1,7 +1,8 @@
 // Captura o dia 
 const date = new Date();
 
-const teste = document
+const teste = document.getElementById('#sayDayAndTime');
+
 
 function todaySDate(dayWeek = 'Segunda-feira', dayMonth = '1', month = 'janeiro', year = '1990', hour = '00:00'){
     // dia semana, dia mês, mês, ano, horas
