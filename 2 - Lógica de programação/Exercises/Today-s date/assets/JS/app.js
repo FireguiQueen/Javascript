03e956059ff42c6e4792df84ea27e2b4e5bbcab9
Guilherme <Fireguiquenbr@gmail.com>
2023-06-18 18:03:09 -0300

    Update app.js

--- 2 - Lógica de programação/Exercises/Today-s date/assets/JS/app.js
+++ 2 - Lógica de programação/Exercises/Today-s date/assets/JS/app.js
@@ -1,54 +1,56 @@
-// Captura o dia 
-const date = new Date();
+// Retorna ao elemento HTML o dia da semana, o dia do mês, o mês, o horário e o ano.
+function sayTime(){
+    const date = new Date();
+    const horasHTML = document.getElementById('sayDayAndTime');
+    horasHTML.innerText = todaySDate();
 
-const teste = document.getElementById('sayDayAndTime');
-teste.innerText = todaySDate();
-
-
-function todaySDate(dayWeek = 'Segunda-feira', dayMonth = '1', month = 'janeiro', year = '1990', hour = '00:00'){
-    // dia semana, dia mês, mês, ano, horas
-    return `${weekDay()}, ${date.getDate()} de ${monthDay()} de ${date.getFullYear()}, às ${correctsTime()}
+    // Retorna o dia da semana, dia do mês, o mês, o ano e as horas.
+    function todaySDate(dayWeek = 'Segunda-feira', dayMonth = '1', month = 'janeiro', year = '1990', hour = '00:00'){
+        // dia semana, dia mês, mês, ano, horas
+        return `${weekDay()}, ${date.getDate()} de ${monthDay()} de ${date.getFullYear()}, às ${correctsTime()}
     `
-}
+    }
 
-// Retorna o dia da semana. Exemplo: segunda-feira
-function weekDay(){
-    const mondayToSunday = [
-        'Domingo',
-        'Segunda-feira',
-        'Terça-feira',
-        'Quarta-feira',
-        'Quinta-feira',
-        'Sexta-feira',
-        'Sábado'
-    ]
-    return mondayToSunday[date.getDay()]
-}
+    // Retorna o dia da semana. Exemplo: segunda-feira
+    function weekDay(){
+        const mondayToSunday = [
+            'Domingo',
+            'Segunda-feira',
+            'Terça-feira',
+            'Quarta-feira',
+            'Quinta-feira',
+            'Sexta-feira',
+            'Sábado'
+        ]
+        return mondayToSunday[date.getDay()]
+    }
 
-// Retorna o mês. Exemplo.: janeiro
-function monthDay(){
-    const dayMonth = [
-        'Janeiro',
-        'Fevereiro',
-        'Março',
-        'Abril',
-        'Maio',
-        'Junho',
-        'Julho',
-        'Agosto',
-        'Setembro',
-        'Outubro',
-        'Novembro',
-        'Dezembro'
-    ]
-    return dayMonth[date.getMonth()]
-}
+    // Retorna o mês. Exemplo.: janeiro
+    function monthDay(){
+        const dayMonth = [
+            'Janeiro',
+            'Fevereiro',
+            'Março',
+            'Abril',
+            'Maio',
+            'Junho',
+            'Julho',
+            'Agosto',
+            'Setembro',
+            'Outubro',
+            'Novembro',
+            'Dezembro'
+        ]
+        return dayMonth[date.getMonth()]
+    }
 
-// Formata horários que possuem zero
-function correctsTime(){
-    const formatHour = date.getHours() < 10? `0${date.getHours()}` : date.getHours();
-    const formatMinutes = date.getMinutes() < 10? `0${date.getMinutes()}` : date.getMinutes();
-    return `${formatHour}:${formatMinutes}`
+    // Formata horários que possuem zero
+    function correctsTime(){
+        const formatHour = date.getHours() < 10? `0${date.getHours()}` : date.getHours();
+        const formatMinutes = date.getMinutes() < 10? `0${date.getMinutes()}` : date.getMinutes();
+        return `${formatHour}:${formatMinutes}:${date.getSeconds()}`
+    }
 }
+sayTime();
 
 
