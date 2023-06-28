488fc78cdc6850735e269be08c488db54e485b27
Guilherme <Fireguiquenbr@gmail.com>
2023-06-28 11:41:46 -0300

    exercise 6: done

--- 2 - Lógica de programação/Exercises/Array - atribuição via desestruturação.js
+++ 2 - Lógica de programação/Exercises/Array - atribuição via desestruturação.js
@@ -40,4 +40,6 @@ console.log(firstPerson, secondPerson)
 // Considere o seguinte array:
 // Atribua os três últimos valores do array a três variáveis diferentes.
 const students = ['John', 'Jane', 'Alex', 'Emily', 'Michael'];
+const [aa, bb,cc, ...rest] = students.slice(-3);
+console.log(aa, bb ,cc, rest)
 
