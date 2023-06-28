1b8c06e1fd63cc357da7f0198442d53f1fce9215
Guilherme <Fireguiquenbr@gmail.com>
2023-06-28 10:52:46 -0300

    Update ARRAY - Atribuição via desestruturação.js

--- 2 - Lógica de programação/ARRAY - Atribuição via desestruturação.js
+++ 2 - Lógica de programação/ARRAY - Atribuição via desestruturação.js
@@ -16,7 +16,11 @@ let pessoa_01 = 'Flora';
 let pessoa_02 = 'Guilherme';
 let pessoa_03 = 'Maria';
 
-// Criamos um array que possui todas as variáveis que dejamos atribuir um novo valor.
+/* 
+    No lado esquerdo, temos o array que armazena variáveis que iremos alterar, no lado direito, 
+    tem um array que contém os valores que serão alocados nas variáveis.
+    É importante notar que a ordem das variáveis deve corresponder à ordem dos valores no array
+*/
 [pessoa_01, pessoa_02, pessoa_03] = ['Gustavo', 'Walter White', 'Jown Snow'];
 
 // Veremos que todas as variáveis agora possuem novos valores
