7cff197c1e92e088b84f366c833120c91cac8d73
Guilherme <Fireguiquenbr@gmail.com>
2023-06-28 11:32:07 -0300

    exercise 3: done

--- 2 - Lógica de programação/Exercises/Array - atribuição via desestruturação.js
+++ 2 - Lógica de programação/Exercises/Array - atribuição via desestruturação.js
@@ -4,6 +4,8 @@ const numbers = [1, 2, 3, 4, 5];
 const [a, b, c, d, e] = numbers;
 console.log(a, b, c, d, e);
 
+
+
 // Considere o seguinte array
 // Atribua os valores do array às variáveis `fruit1`, `fruit2`, `fruit3` e `fruit4`.
 const fruits = ['apple', 'banana', 'orange', 'kiwi'];
@@ -11,9 +13,14 @@ const [fruit1, fruit2, fruit3, fruit4] = fruits;
 console.log(fruit1, fruit2, fruit3);
 
 
+
 // Considere o seguinte array
 // Atribua o primeiro valor do array à variável `primaryColor` e o restante do array à variável `secondaryColors`.
 const colors = ['red', 'blue', 'green'];
+const [primaryColor, ...secondaryColors] = colors;
+console.log(primaryColor, secondaryColors); 
+
+
 
 // Considere o seguinte array:
 // Atribua o primeiro valor do array à variável `x`, o segundo valor à variável `y` e o terceiro valor à variável `z`.
