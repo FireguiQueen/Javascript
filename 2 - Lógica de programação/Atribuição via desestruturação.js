d58c8ff58aaabea04539f087ef00e1f6fff25151
Guilherme <Fireguiquenbr@gmail.com>
2023-06-28 11:07:26 -0300

    Update Atribuição via desestruturação.js

--- 2 - Lógica de programação/Atribuição via desestruturação.js
+++ 2 - Lógica de programação/Atribuição via desestruturação.js
@@ -1,4 +1,4 @@
-// Atribuição via desestruturação
+//  ATRIBUIÇÃO VIA DESESTRUTURAÇÃO
 /*
     A atribuição via desestruturação é uma sintaxe em JavaScript que permite extrair valores 
     de arrays ou objetos e atribuí-los a variáveis individuais de forma concisa. 
@@ -6,22 +6,23 @@
     propriedades específicas de um objeto.
 */
 
-// Atribuir novos valores a variáveis é algo bem simples
-let pessoa = 'Gustavo'; 
-pessoa = 'Maria'; 
 
+// Atribuir novos valores a variáveis é algo bem simples
+let pessoa = 'João'; 
+pessoa = 'Maria'; 
 
 // Mas e se tivermos diversas variáveis que precisam possuir novos valores?
 let pessoa_01 = 'Flora';
-let pessoa_02 = 'Guilherme';
+let pessoa_02 = 'Otavio';
 let pessoa_03 = 'Maria';
 
+
 /* 
-    No lado esquerdo, temos o array que armazena variáveis que iremos alterar, no lado direito, 
-    tem um array que contém os valores que serão alocados nas variáveis.
+    No lado esquerdo, temos o array que armazena as variáveis que iremos alterar. 
+    No lado direito, temos array que contém os valores que serão alocados nas variáveis.
     É importante notar que a ordem das variáveis deve corresponder à ordem dos valores no array
 */
-[pessoa_01, pessoa_02, pessoa_03] = ['Gustavo', 'Walter White', 'Jon Snow'];
+[pessoa_01, pessoa_02, pessoa_03] = ['Chloe', 'Walter White', 'Jon Snow'];
 
 // Veremos que todas as variáveis agora possuem novos valores
 console.log(pessoa_01, pessoa_02, pessoa_03)
