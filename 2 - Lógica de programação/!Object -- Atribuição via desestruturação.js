0158e6144d37524c5ec705366eac007ccb19b11c
Guilherme <Fireguiquenbr@gmail.com>
2023-06-28 14:31:03 -0300

    Update !Object -- Atribuição via desestruturação.js

---
+++ 2 - Lógica de programação/!Object -- Atribuição via desestruturação.js
@@ -0,0 +1,15 @@
+//  ATRIBUIÇÃO VIA DESESTRUTURAÇÃO
+/*
+    A atribuição via desestruturação é uma sintaxe em JavaScript que permite extrair valores 
+    de arrays ou objetos e atribuí-los a variáveis individuais de forma concisa. 
+    Isto é útil quando você deseja atribuir vários valores de uma vez ou quando deseja extrair 
+    propriedades específicas de um objeto.
+*/
+
+/;
+const teste = {
+    name: 'oi'
+    
+}
+
+console.log(teste.name)
