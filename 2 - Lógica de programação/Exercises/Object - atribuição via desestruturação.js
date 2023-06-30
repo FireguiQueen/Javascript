5d4a195b95c134fe40567f131035a7a7d3c6d9c4
Guilherme <Fireguiquenbr@gmail.com>
2023-06-29 22:18:02 -0300

    Update Object - atribuição via desestruturação.js

--- 2 - Lógica de programação/Exercises/Object - atribuição via desestruturação.js
+++ 2 - Lógica de programação/Exercises/Object - atribuição via desestruturação.js
@@ -65,32 +65,3 @@ const {fullName, position, department} = employee;
 
 
 
-// ********************************************************* // 
-console.log('-----------------------------------')
-// ********************************************************* // 
-
-
-
-// Crie um objeto relacionado a um funcionário, o mesmo deve possuir muitas informações. 
-// Crie uma função que o capture e devolve os valores das propriedades de maneira "mastigada".
-const worker = {
-    nameSurname: 'Adam Doe Lemster',
-    gender: 'male',
-    age: 36,
-    NIN: 'AB123456C',
-    address: {
-        city: 'Ely',
-        postcode: 'CB61SA',
-        house: 42
-    },
-
-    companyDerpartment: 'Marketing / design',
-    worker_id(){
-        const date = new Date();
-        const year = date.getFullYear();
-        return `${year - this.age}.${this.NIN}`;
-    },
-
-}
-
-console.log(worker)
