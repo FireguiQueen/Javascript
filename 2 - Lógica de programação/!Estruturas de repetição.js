96793ca03994dcb8d663769f648202547b1223fc
Guilherme <Fireguiquenbr@gmail.com>
2023-06-29 21:30:50 -0300

    Update !Estruturas de repetição.js

---
+++ 2 - Lógica de programação/!Estruturas de repetição.js
@@ -0,0 +1,13 @@
+// FOR 
+/*  
+    1. Criar uma variável e atribuir um valor a ela; 
+    2. Criar uma condição (caso seja verdadeira, a repetição irá continuar);
+    3. Incrementar/decrementar o valor de nossa variável.
+
+    >> Esses passos são separados por ponto e vírgula dentro dos parêntesis.
+*/
+    
+// Foi dito o seguinte: I é igual a zero. I é menor do que 10? Se sim, adicone +1 à variável I.
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
