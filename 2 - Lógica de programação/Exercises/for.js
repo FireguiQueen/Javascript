05b7a5be7def7f8eb5ebc5a483fcdc6d0909baa5
Guilherme <Fireguiquenbr@gmail.com>
2023-07-04 23:55:11 -0300

    Update for.js

--- 2 - Lógica de programação/Exercises/for.js
+++ 2 - Lógica de programação/Exercises/for.js
@@ -15,3 +15,25 @@ const numeros = [0, 23, 52, 56, 73, 82, 91, 107, 143, 167];
 for(let i = 0; i < numeros.length; i++){
     console.log(`${numeros[i]} - ${numeros[i] % 2 !== 0? 'Impar' : 'Par' }`)
 }
+
+
+// Com base no array abaixo, pegue os impares e pares e jogue os para outros arrays
+function imparPar(){
+    const numbers = [0, 23, 52, 56, 73, 82, 91, 107, 143, 167];
+    const impares = [];
+    const pares = [];
+    for(let i = 0; i <numbers.length; i++){
+        if(i % 2 === 0 ){
+            pares.push(i)
+        } else {
+            impares.push(i)
+        }
+    }
+    return `
+    Impares = ${impares}
+    Pares = ${pares}
+    `
+} 
+console.log(imparPar())
+
+
