0ec76fb9bb3664202b4b2e2f3e57b97cc238e943
Guilherme <Fireguiquenbr@gmail.com>
2023-07-04 23:39:01 -0300

    update

--- 2 - Lógica de programação/Exercises/for.js
+++ 2 - Lógica de programação/Exercises/for.js
@@ -13,5 +13,5 @@ for(let i = -10; i <= 5; i++){
 // Com base no array abaixo, determine se o elemento é par ou ímpar
 const numeros = [0, 23, 52, 56, 73, 82, 91, 107, 143, 167];
 for(let i = 0; i < numeros.length; i++){
-    console.log(`${numeros[i] % 2 }`)
+    console.log(`${numeros[i]} - ${numeros[i] % 2 !== 0? 'Impar' : 'Par' }`)
 }
