3fa6f7f2bcf56dbe241ebb961ccd2cecd89f1e2a
Guilherme <Fireguiquenbr@gmail.com>
2023-07-04 22:55:59 -0300

    Update !Estruturas de repetição.js

--- 2 - Lógica de programação/!Estruturas de repetição.js
+++ 2 - Lógica de programação/!Estruturas de repetição.js
@@ -5,13 +5,20 @@
     se a repetição deve continuar ou não.
 
     2. Criar uma condição (caso seja verdadeira, a repetição irá continuar);
-    > Esta condição faz uso da nossa variável de controle
+    > Esta condição faz uso da nossa variável de controle.
 
     3. Incrementar/decrementar o valor de nossa variável.
 
     >> Esses passos são separados por ponto e vírgula dentro dos parêntesis.
 */
+
+
+// Por que variáveis de controle são chamadas de "I"?
+/*
+    A letra "i" nesses casos significa "indice".
+*/
     
+
 // Foi dito o seguinte: I é igual a zero. I é menor do que 10? Se sim, adicone +1 à variável I.
 for(let i = 0; i < 10; i++){
     console.log(i)
