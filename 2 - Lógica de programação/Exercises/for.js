be18fef6980b51445e18617d7db21b44d2845a1f
Guilherme <Fireguiquenbr@gmail.com>
2023-07-04 23:21:13 -0300

    Update for.js

---
+++ 2 - Lógica de programação/Exercises/for.js
@@ -0,0 +1,7 @@
+// Com base no array abaixo, indique o indice de cada fruta.
+const fruit_list = ['Banana', 'Manga', 'Goiaba', 'Blue Barry', 'Morango']
+for(let i = 0; i < fruit_list.length; i++){
+    console.log(`${i} - ${fruit_list[i]}`)
+}
+
+// 
