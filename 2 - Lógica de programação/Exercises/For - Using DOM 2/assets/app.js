a50a2e1b3b4a74ebc7e513c96a145172acc1d60a
Guilherme <Fireguiquenbr@gmail.com>
2023-07-07 16:27:06 -0300

    update

--- 2 - Lógica de programação/Exercises/For - Using DOM 2/assets/app.js
+++ 2 - Lógica de programação/Exercises/For - Using DOM 2/assets/app.js
@@ -1,14 +1,72 @@
-const elementos = [
-    {tag: 'p', texto: 'Frase 1'},
-    {tag: 'div', texto: 'Frase 2'},
-    {tag: 'footer', texto: 'Frase 3'},
-    {tag: 'section', texto: 'Frase 4'}
+let frutas = [
+    "abacaxi",
+    "banana",
+    "caju",
+    "damasco",
+    "figo",
+    "goiaba",
+    "laranja",
+    "manga",
+    "uva",
+    "melancia",
+    "melão",
+    "morango",
+    "pêssego",
+    "abacate",
+    "ameixa",
+    "cereja",
+    "kiwi",
+    "limão",
+    "maçã",
+    "pera",
+    "tangerina",
+    "maracujá",
+    "acerola",
+    "jabuticaba",
+    "abacaba",
+    "caqui",
+    "framboesa",
+    "amora",
+    "melão",
+    "carambola",
+    "mamão",
+    "jaca",
+    "pitanga",
+    "graviola",
+    "romã",
+    "guaraná",
+    "cupuaçu",
+    "cajá",
+    "cambuci",
+    "mexerica",
+    "pequi",
+    "lichia",
+    "tamarindo",
+    "abiu",
+    "bacuri",
+    "taperebá",
+    "umbu",
+    "murici",
+    "araticum",
+    "jambo",
+    "cupuaíçu"
 ];
+  
 
-const writeOn = document.querySelector('main');
-for(let i = 0; i < elementos.length; i++){
-    let {tag, texto} = elementos[i]
-    writeOn.innerHTML += `<${tag}> ${texto} </${tag}`
+let atraso = 0;
+const writeOn = document.querySelector('.lista');
+function write(fruta){
+    atraso += 0.08;
+    
+    return `
+        <div class='cu' style="animation: appear .5s linear forwards ${atraso}s";>> 
+            ${fruta} 
+        </div>
+    `
+
+}
+for(let i = 0; i < frutas.length; i++){
+    writeOn.innerHTML += write(frutas[i]);
 }
 
 
