41bdd988ac5ab866107312f9c19f5b84501764bd
Guilherme <Fireguiquenbr@gmail.com>
2023-07-06 21:52:42 -0300

    Update app.js

--- 2 - Lógica de programação/Exercises/For - Using DOM/assets/app.js
+++ 2 - Lógica de programação/Exercises/For - Using DOM/assets/app.js
@@ -3,4 +3,10 @@ const elementos = [
     {tag: 'div', texto: 'Frase 2'},
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'}
-]
+];
+
+
+const writeOn = document.querySelector()
+for(let i = 0; i < elementos.length; i++){
+    
+}
