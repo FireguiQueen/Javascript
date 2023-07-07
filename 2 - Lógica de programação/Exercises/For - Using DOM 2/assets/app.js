fc8af19359edd8f6f78134543e7c3cac70a22b26
Guilherme <Fireguiquenbr@gmail.com>
2023-07-07 15:49:31 -0300

    update

---
+++ 2 - Lógica de programação/Exercises/For - Using DOM 2/assets/app.js
@@ -0,0 +1,14 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'}
+];
+
+const writeOn = document.querySelector('main');
+for(let i = 0; i < elementos.length; i++){
+    let {tag, texto} = elementos[i]
+    writeOn.innerHTML += `<${tag}> ${texto} </${tag}`
+}
+
+
