eed17cb651d813413e0bdb4b55273082e5d18b94
Guilherme <Fireguiquenbr@gmail.com>
2023-07-10 20:16:01 -0300

    Create !For in & For of.js

---
+++ 2 - Lógica de programação/!For in & For of.js
@@ -0,0 +1,28 @@
+// FOR IN 
+/*
+    For in irá sempre nos retornar números. 
+    Por exemplo, caso você utilize do For in em uma string, ele irá retornar a quantidade
+    de caracteres que esta string possui.
+    Porém uma de cada vez, permitindo que você acesse o valor deste caractere.
+
+    Se você utilizar o For in em um objeto, ele irá retornar a quantidade de propriedades 
+    que o mesmo possui.
+    E se for um array, ele irá retornar a quantidade de elementos que este array possui.
+*/
+
+
+// SEGUNDO MÉTODO PARA ACESSAR VALORES DE PROPRIEDADES DE UM OBJETO.
+/*
+    Por padrão, nós temos o método "tradicional", onde colocamos um ponto final (.)
+    e passamos o nome da propriedade que desejamos pegar o valor. Por exemplo:
+*/
+const pessoa = {
+    name: 'Victoria'
+}
+console.log(pessoa.name)
+
+// P
+
+
+
+
