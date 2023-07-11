d4c7f2655a3d914d43fb225c08b4cf01a5f5afd5
Guilherme <Fireguiquenbr@gmail.com>
2023-07-10 21:01:37 -0300

    Create for in.js

---
+++ 2 - Lógica de programação/Exercises/for in.js
@@ -0,0 +1,23 @@
+// Imprima todas as propriedades e valores do objeto carro
+const carro = {
+    marca: 'Toyota',
+    modelo: 'Corolla',
+    ano: 2022,
+    cor: 'Prata'
+};
+  
+
+
+// Conte o número de propriedades do objeto aluno
+const aluno = {
+    nome: 'João',
+    idade: 20,
+    curso: 'Engenharia',
+    universidade: 'Universidade XYZ'
+};
+
+
+
+
+// Imprima os índices e valores do array de frutas
+const frutas = ['maçã', 'banana', 'laranja', 'uva'];
