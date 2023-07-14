9ea7f75e843d74411b9c0c935acd0f68a6e2bc16
Guilherme <Fireguiquenbr@gmail.com>
2023-07-14 11:40:01 -0300

    a

--- 2 - Lógica de programação/!For in & For of.js
+++ 2 - Lógica de programação/!For in & For of.js
@@ -1,15 +1,7 @@
 // FOR IN 
 /*
-    Se você usar o loop for...in com uma string, ele irá iterar sobre cada caractere da string. 
-    A cada iteração, ele atribui o valor do caractere à variável especificada e executa o bloco 
-    de código.
-
-    Se você usar o for...in em um objeto, ele irá iterar sobre as propriedades do objeto. 
-    A cada iteração, ele atribui o nome da propriedade à variável especificada e executa o 
-    bloco de código.
-
-    Se você usar o for...in em um array, ele irá iterar sobre os índices do array. 
-    A cada iteração, ele atribui o índice à variável especificada e executa o bloco de código.
+    O for..in serve para nós iteramos sobre cada propriedade enumerável.
+    Por
 
     No exemplo abaixo, o for...in iterará sobre as propriedades do objeto "cachorro".
     A cada iteração, ele atribuirá o nome da propriedade à variável "informacoes"
---
+++ 2 - Lógica de programação/Exercises/tsr.js
@@ -0,0 +1,4 @@
+const array = ['oi', 'mds', 'bacana', 'juju', 'ola' ,  'not cool']
+for(let elements in array){
+ console.log(array[elements])
+}
