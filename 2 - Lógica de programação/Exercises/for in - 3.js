6b18292ac8d9e29c35e102ff711ddad78327a2a9
Guilherme <Fireguiquenbr@gmail.com>
2023-07-15 23:04:24 -0300

    update

--- 2 - Lógica de programação/Exercises/for in - 3.js
+++ 2 - Lógica de programação/Exercises/for in - 3.js
@@ -1,35 +1,24 @@
-// Objeto com algumas propriedades
+// OBJETO COM ALGUMAS PROPRIEDADES
 const pessoa = {
     nome: 'João',
     idade: 30,
     profissao: 'Engenheiro'
-  };
-  
-  // Usando o for...in para percorrer as propriedades do objeto
-  for (const propriedade in pessoa) {
-    console.log(`${propriedade}: ${pessoa[propriedade]}`);
-  }
+};
+// USANDO O FOR...IN PARA PERCORRER AS PROPRIEDADES DO OBJETO
+
 
   
-
-
-
 // Função que conta a ocorrência de cada letra em uma string
-function contarLetras(str) {
-    const contagem = {};
-  
-    // Usando o for...in para percorrer os caracteres da string
-    for (const char in str) {
-      const letra = str[char].toLowerCase();
-  
-      if (letra !== ' ') {
-        contagem[letra] = (contagem[letra] || 0) + 1;
-      }
-    }
-  
-    return contagem;
-  }
-  
-  const resultado = contarLetras('Hello World');
-  console.log(resultado);
+
+
+
+
+// Objeto representando a lista de compras
+const listaDeCompras = {
+    banana: 2,
+    maçã: 5,
+    laranja: 3,
+    pão: 2,
+    leite: 1
+  };
   
