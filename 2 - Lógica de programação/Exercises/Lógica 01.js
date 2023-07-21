1693df1729e59eb1b57360f2af300b40311abd86
Guilherme <Fireguiquenbr@gmail.com>
2023-07-21 15:48:02 -0300

    Update Lógica 01.js

--- 2 - Lógica de programação/Exercises/Lógica 01.js
+++ 2 - Lógica de programação/Exercises/Lógica 01.js
@@ -6,6 +6,6 @@ console.log(maxNum(6,4))
 
 // Escreva uma função que possua dois parâmetros: altura e largura. Retorne true se ela estiver em modo paisagem.
 function ePaisagem(height, width){
-    return width < height
+    return `Está em modo paisagem? ${width < height}`
 }
 console.log(ePaisagem(1,5))
