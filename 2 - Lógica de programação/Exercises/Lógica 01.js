b508ee68b208edfbb7025fa1b3fe6f2df190dc8b
Guilherme <Fireguiquenbr@gmail.com>
2023-07-21 15:59:41 -0300

    Update Lógica 01.js

--- 2 - Lógica de programação/Exercises/Lógica 01.js
+++ 2 - Lógica de programação/Exercises/Lógica 01.js
@@ -6,5 +6,21 @@ console.log(maxNum(6,4))
 
 // Escreva uma função que possua dois parâmetros: altura e largura. Retorne true se ela estiver em modo paisagem.
 const ePaisagem = (height, width) => `Está em modo paisagem? ${width < height}`
-
 console.log(ePaisagem(1,5))
+
+
+// Escreva uma função que recebe um númmero. Retorne o seguinte.:
+// Número é divisível por 3 = Fizz 
+// Número é divisível por 5 = Buzz
+// Número é divisível por 3 e 5 = FizzBuzz
+// Número não é divisível por 3 e 5 = Retorna ele mesmo
+function checkNum(num){
+    if(Number.isInteger(num)){
+        if(num % 3 === 0 && num % 5 === 0) return 'FizzBuzz'; 
+        if(num % 3 === 0) return 'Fizz';
+        if(num % 5 === 0) return 'Buzz';
+        return num
+    }
+    return 'Passe um número como argumento!'
+}
+console.log(checkNum(''))
