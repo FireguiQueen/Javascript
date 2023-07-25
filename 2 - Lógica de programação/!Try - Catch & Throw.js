d12881288f49a4c65512d636c5ce2c5916086629
Guilherme <Fireguiquenbr@gmail.com>
2023-07-25 12:36:09 -0300

    Update !Try - Catch & Throw.js

--- 2 - Lógica de programação/!Try - Catch & Throw.js
+++ 2 - Lógica de programação/!Try - Catch & Throw.js
@@ -15,7 +15,7 @@
 
     - Exibir uma mensagem de erro amigável para o usuário, informando sobre o ocorrido e sugerindo uma ação para corrigir o problema.
     - Gravar informações sobre a exceção em um arquivo de log para posterior análise e depuração do erro.
-    - Tentar novamente a operação que causou a exceção, em uma nova tentativa para evitar o erro.
+    - Tentar novamente a operação que causou o erro, em uma nova tentativa para evitar o erro.
     - Utilizar um valor padrão ou uma alternativa caso o erro ocorra em uma situação não crítica.
     - Notificar o administrador do sistema sobre o erro para que medidas corretivas possam ser tomadas.
 */
