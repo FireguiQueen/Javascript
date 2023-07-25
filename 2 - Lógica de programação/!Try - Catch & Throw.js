f7c1bcd5225692f2aff470ce8a8b8d4870132289
Guilherme <Fireguiquenbr@gmail.com>
2023-07-25 12:34:49 -0300

    Update !Try - Catch & Throw.js

--- 2 - Lógica de programação/!Try - Catch & Throw.js
+++ 2 - Lógica de programação/!Try - Catch & Throw.js
@@ -8,10 +8,16 @@
     E caso ocorra algum erro dentro do try (por consequência do código arriscado), o `catch`
     é acionado para tratar o erro e evitar que o programa seja encerrado.
     
-    Em português, 'catch' significa 'pegar', neste caso, estamos pegando o erro causado pelo
-    código que está no 'try' e tratando o mesmo.
-    Tratar o erro pode ser um simples ato de fazer o programa não se encerrar, ou até mesmo
-    invocar um função que 
+    Em português, 'catch' significa 'pegar'. O 'catch' está pegando o erro ocasionado pelo código
+    do bloco 'try' e tratando o mesmo. 
+    O tratamento de erro pode variar desde uma simples ação para evitar a interrupção do 
+    programa até a execução de ações específicas para lidar com a exceção de forma adequada. 
+
+    - Exibir uma mensagem de erro amigável para o usuário, informando sobre o ocorrido e sugerindo uma ação para corrigir o problema.
+    - Gravar informações sobre a exceção em um arquivo de log para posterior análise e depuração do erro.
+    - Tentar novamente a operação que causou a exceção, em uma nova tentativa para evitar o erro.
+    - Utilizar um valor padrão ou uma alternativa caso o erro ocorra em uma situação não crítica.
+    - Notificar o administrador do sistema sobre o erro para que medidas corretivas possam ser tomadas.
 */
 
 // Sintaxe
