7c6c4f96eec34b0015f0219b564670518f000156
Guilherme <Fireguiquenbr@gmail.com>
2023-07-25 12:32:09 -0300

    Update !Try - Catch & Throw.js

--- 2 - Lógica de programação/!Try - Catch & Throw.js
+++ 2 - Lógica de programação/!Try - Catch & Throw.js
@@ -6,7 +6,12 @@
     O 'try' é usado para tentarmos rodar um código arriscado ao nosso programa, onde o mesmo
     pode causar erros e interromper a execução de todo o nosso programa. 
     E caso ocorra algum erro dentro do try (por consequência do código arriscado), o `catch`
-    é acionado para tratar o erro e evitar que o programa seja encerrado. 
+    é acionado para tratar o erro e evitar que o programa seja encerrado.
+    
+    Em português, 'catch' significa 'pegar', neste caso, estamos pegando o erro causado pelo
+    código que está no 'try' e tratando o mesmo.
+    Tratar o erro pode ser um simples ato de fazer o programa não se encerrar, ou até mesmo
+    invocar um função que 
 */
 
 // Sintaxe
@@ -17,3 +22,4 @@ try{
 }
 
 // No bloco try, iremos colocar o nosso código potencialmente perigoso (ou seja, pode gerar erros) ao programa 
+// No bloco catch, 
