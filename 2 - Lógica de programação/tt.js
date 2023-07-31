0d1d32381e6b9ad4b8084f6dfbfc59322e5706ff
Guilherme <Fireguiquenbr@gmail.com>
2023-07-30 23:32:50 -0300

    update

--- 2 - Lógica de programação/tt.js
+++
@@ -1,3 +0,0 @@
-const data = new Date(2020,0);
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric'}
-console.log(data.toLocaleDateString('pt-br', options))
