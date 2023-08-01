5e33d460c844c3738bb0efddff65acc603e6154b
Guilherme <Fireguiquenbr@gmail.com>
2023-07-31 23:05:20 -0300

    update

--- 2 - Lógica de programação/teste.js
+++ 2 - Lógica de programação/teste.js
@@ -1,6 +1,6 @@
-let segundo = 0;
-const data = new Date()
-setInterval(function(){
-    segundo++
-    console.log(new Date(segundo * 1000))
-}, 1000)
+const data = new Date();
+
+const config = {
+    hour12: 'false'
+}
+console.log(data.toLocaleTimeString('pt-br', config))
