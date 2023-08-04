97f079fafdb6caf3ff3ddbc1fb684de7f5ad427e
Guilherme <Fireguiquenbr@gmail.com>
2023-08-04 11:13:21 -0300

    Update app.js

--- 2 - Lógica de programação/Exercises/Tasks/app.js
+++ 2 - Lógica de programação/Exercises/Tasks/app.js
@@ -1,30 +1,29 @@
 function tasksToDo(){
-    const tasks = [];
 
     const botoes = document.querySelectorAll('.btn')
     const [taskTyped, sendTask] = botoes;
     
-    const createLi = document.createElement('li');
     const deleteTask = document.createElement('button');
     
     function addTask(task){
+        const createLi = document.createElement('li');
         document.querySelector('.task-list').appendChild(createLi).innerText = task;
-
         taskTyped.value = '';
-
-        tasks.push({
-            keyword: task, 
-
-        }); console.log(tasks)
+        taskTyped.focus()
     }
+    
+    taskTyped.addEventListener('keypress', function(e){
+        const task = e.target
+        if(!taskTyped.value) return;      
+        if(e.keyCode === 13) addTask(task.value);
+    })
 
     document.addEventListener('click', function(e){
         const whichBtn = e.target;
         if(!taskTyped.value) return;      
 
-        if(whichBtn === sendTask){
-            addTask(taskTyped.value);
-        }
+        if(whichBtn === sendTask) addTask(taskTyped.value);
+        
     })
 
 } tasksToDo()
