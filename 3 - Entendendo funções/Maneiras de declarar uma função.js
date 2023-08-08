5b9fc2773eb3d89e03b42cfd234790d4d47c28e8
Guilherme <Fireguiquenbr@gmail.com>
2023-08-07 22:22:39 -0300

    Create Maneiras de declarar uma função.js

---
+++ 3 - Entendendo funções/Maneiras de declarar uma função.js
@@ -0,0 +1,11 @@
+// DECLARATION
+// Colocamos a keyword "function" e a idêntificamos com um nome;
+// E este nome seria justamente a "declaração", já que não é uma função anônima;
+// Unico tipo declaração que ocorre o hoisting.  
+function sayHi(){
+    console.log('hi')
+}
+
+
+// Expression
+// 
