8fadd43b06591810d765de7dd8d8ec644dddf4d6
Guilherme <Fireguiquenbr@gmail.com>
2023-08-08 22:11:16 -0300

    Update Parameters.js

--- 3 - Entendendo funções/Parameters.js
+++ 3 - Entendendo funções/Parameters.js
@@ -4,7 +4,7 @@
     ocorre.
 
     Ao passar argumentos para funções sem parâmetros, esses argumentos serão armazenados
-    em uma variável chamada "arguments" --> CASO A FUNÇÃO SEJA DE DECLARAÇÃO: function exemplo(){}
+    em uma variável chamada "arguments" (apenas para funções que fazem uso da keyword function).
 
     Independente se o argumento será passado para um parâmetro ou não, ele irá ir para a variável
     chamada "arguments".
