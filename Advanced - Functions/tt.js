1a5c3c2a9d1d11557b39816ec4cc48f2fa3b0c78
Guilherme <Fireguiquenbr@gmail.com>
2023-08-12 18:43:51 -0300

    Update tt.js

--- Advanced - Functions/tt.js
+++ Advanced - Functions/tt.js
@@ -1,29 +1,30 @@
-function whatToDo(parOrImpar, n){
-    const numbers = [];
+// function whatToDo(parOrImpar, n){
+//     const numbers = [];
 
-    if(parOrImpar === 'onlyPar'){
-        for(let i of n){
-            if(i % 2 === 0){
-                numbers.push(i)
-                continue;
-            }
-        }
-        return numbers.sort();
-    }
+//     if(parOrImpar === 'onlyPar'){
+//         for(let i of n){
+//             if(i % 2 === 0){
+//                 numbers.push(i)
+//                 continue;
+//             }
+//         }
+//         return numbers.sort();
+//     }
 
-    if(parOrImpar === 'onlyImpar'){
-        for(let i of n){
-            if(i % 2 !== 0){
-                numbers.push(i)
-                continue;
-            }
-        }
-        return numbers.sort();
-    }
-}
+//     if(parOrImpar === 'onlyImpar'){
+//         for(let i of n){
+//             if(i % 2 !== 0){
+//                 numbers.push(i)
+//                 continue;
+//             }
+//         }
+//         return numbers.sort();
+//     }
+// }
 
-function parImpar(which, ...numbers){
-    return whatToDo(which, numbers)
-}
+// function parImpar(which, ...numbers){
+//     return whatToDo(which, numbers)
+// }
 
-console.log(parImpar('onlyImpar', 222, 10, 5))
+// console.log(parImpar('onlyImpar', 222, 10, 5))
+
