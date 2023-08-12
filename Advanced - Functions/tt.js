72376949f71c8ce77c4fb941cb0a37959b55087c
Guilherme <Fireguiquenbr@gmail.com>
2023-08-12 19:41:44 -0300

    Create tt.js

---
+++ Advanced - Functions/tt.js
@@ -0,0 +1,30 @@
+// function whatToDo(parOrImpar, n){
+//     const numbers = [];
+
+//     if(parOrImpar === 'onlyPar'){
+//         for(let i of n){
+//             if(i % 2 === 0){
+//                 numbers.push(i)
+//                 continue;
+//             }
+//         }
+//         return numbers.sort();
+//     }
+
+//     if(parOrImpar === 'onlyImpar'){
+//         for(let i of n){
+//             if(i % 2 !== 0){
+//                 numbers.push(i)
+//                 continue;
+//             }
+//         }
+//         return numbers.sort();
+//     }
+// }
+
+// function parImpar(which, ...numbers){
+//     return whatToDo(which, numbers)
+// }
+
+// console.log(parImpar('onlyImpar', 222, 10, 5))
+
