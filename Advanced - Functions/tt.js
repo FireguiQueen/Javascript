2ae44113e55b75d6758c5719ea6601f90b192f60
Guilherme <Fireguiquenbr@gmail.com>
2023-08-12 20:13:57 -0300

    Update tt.js

--- Advanced - Functions/tt.js
+++ Advanced - Functions/tt.js
@@ -1,14 +1,44 @@
-function criarContador() {
-    let contador = 0;
-  
-    function incrementar() {
-      contador++;
-      console.log(contador);
-      return contador;
-    }
-  
-    return incrementar();  // Aqui, a função incrementar é imediatamente invocada e seu resultado é retornado.
-  }
-  
-  const resultado = criarContador();  // resultado conteria o valor 1, pois a função incrementar foi invocada imediatamente.
-      
+// function whatToDo(parOrImpar, n){
+//     const numbers = [];
+
+//     if(parOrImpar === 'onlyPar'){
+//         for(let i of n){
+//             if(i % 2 === 0){
+//                 numbers.push(i)
+//                 continue;
+//             }
+//         }
+//         return numbers.sort();
+//     }
+
+//     if(parOrImpar === 'onlyImpar'){
+//         for(let i of n){
+//             if(i % 2 !== 0){
+//                 numbers.push(i)
+//                 continue;
+//             }
+//         }
+//         return numbers.sort();
+//     }
+// }
+
+// function parImpar(which, ...numbers){
+//     return whatToDo(which, numbers)
+// }
+
+// console.log(parImpar('onlyImpar', 222, 10, 5))
+
+function somar(){
+    let x = 0;
+
+    function incrementar(){
+        x++
+        return x
+    } 
+
+    return incrementar  
+}
+
+const somarX = somar()
+console.log(somarX())
+console.log(somarX())
