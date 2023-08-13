8340a1af4d96f068a3827575efe93f8dfcd795dc
Guilherme <Fireguiquenbr@gmail.com>
2023-08-13 11:12:34 -0300

    Update tt.js

--- Advanced - Functions/tt.js
+++ Advanced - Functions/tt.js
@@ -1,12 +1,1 @@
-let x = 3000000000
-let mee = []
-while(x > 0){
-    x--
-    let i = Math.random() * (5001 - 2000) + 2000;
-    if(i === 5000 || i >= 5000){
-        mee.push(i);
-        continue;
-    }
-}
-
-console.log(mee)
+console.log(Math.random() * 20)
